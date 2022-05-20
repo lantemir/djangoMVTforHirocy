4ecd89418466e7632b8bea2f06ba5015eeb22109
lantemir <temiros@mail.ru>
2022-05-20 21:17:35 +0600

    make it better

--- app_student/urls.py
+++ app_student/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+from .views import vue
+
+
+
 urlpatterns = [
+    path('vue/', vue, name="vue"),
 ]
--- app_student/views.py
+++ app_student/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def vue(request):
+    return render(request, 'app_teacher/pages/index.html')
+
+
